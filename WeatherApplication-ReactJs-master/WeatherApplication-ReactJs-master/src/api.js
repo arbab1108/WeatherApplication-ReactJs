839e6268ffb86de3b4ea5e1f1a5e8e93ad3daf24
Arbab Khan <125142755+arbab1108@users.noreply.github.com>
2024-01-07 02:39:26 +0530

    Add files via upload

---
+++ WeatherApplication-ReactJs-master/WeatherApplication-ReactJs-master/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '55e03c8ec4msh85abc14d5976da4p1afb71jsnf880277a5140',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8dc61a6bae28bc88191bedb3bee7fa95";
